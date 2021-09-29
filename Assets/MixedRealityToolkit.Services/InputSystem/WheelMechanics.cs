884dfd25190d4d842a00e809373e16970fed84e7
ETBIMlab <70665547+ETBIMlab@users.noreply.github.com>
2021-09-29 15:32:56 -0700

    MRTK import

---
+++ Assets/MixedRealityToolkit.Services/InputSystem/WheelMechanics.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelMechanics : MonoBehaviour
+{
+    //This represents rotational speed
+    float rotSpeed = 0; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Once selected the wheel should spin
+        if(Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+        }
+        transform.Rotate(0, rotSpeed, 0);
+
+        //Added for the speed to slow down
+        this.rotSpeed *= 0.96f;
+    }
+}
